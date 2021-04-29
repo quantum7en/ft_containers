f7dca4f855cb076f17cc71a091c3eebd77213eb9
Phylicia Melinda <pmelinda@oa-g3.msk.21-school.ru>
2021-04-29 22:08:09 +0300

    add

---
+++ ft_list.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Phylicia Melinda on 4/29/21.
+//
+
+#include "ft_list.hpp"
---
+++ ft_list.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Phylicia Melinda on 4/29/21.
+//
+
+#ifndef FT_CONTAINERS_FT_LIST_HPP
+#define FT_CONTAINERS_FT_LIST_HPP
+
+//template < class T, class Alloc = allocator<T> > class list;
+namespace ft {
+	template<typename T>
+	typedef T value_type;
+	class ft_list {
+	public:
+
+	};
+}
+
+
+#endif //FT_CONTAINERS_FT_LIST_HPP
